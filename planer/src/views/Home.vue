1a528a412dcf0cc9806b1694fe40a90325360329
Daniel <danielselga@gmail.com>
2021-07-22 00:34:21 -0300

    starting project frist column finished

---
+++ planer/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SingleProject from '../components/SingleProject.vue'
+
+export default {
+  name: 'Home',
+  components: {SingleProject},
+  data(){
+    return {
+      projects: []
+    }
+  },
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message))
+  }
+}
+</script>
+
+<style>
+  .project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+    border-left: 4px solid #e90074;
+  }
+
+  h3 {
+    cursor: pointer;
+  }
+
+
+</style>
